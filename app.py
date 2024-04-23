6f58fd21706395941c46362c766c7b74e3cd96e4
acvera <131630334+acvera@users.noreply.github.com>
2024-04-23 14:47:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def main():
    
     @st.cache_resource
     def load_model():
-        model = tf.keras.models.load_model('dog_cat_classifier.hdf5')
+        model = keras.models.load_model('dog_cat_classifier.hdf5')
         return model
     
     def import_and_predict(image_data, model):
